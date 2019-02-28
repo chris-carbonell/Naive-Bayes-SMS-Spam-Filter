d0e6648138bd48b6b95630ecd6973af4f8c0c5bc
Carbonell <ccarbone@amfam.com>
2019-02-28 15:12:16 -0600

    data imported correctly

--- 02_scripts/01_scrub.py
+++ 02_scripts/01_scrub.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
 
     # can't read CSV with read_csv
-    #df_data = pd.read_csv("../01_data/SMSSpamCollection.csv", header=None)
+    # df_data = pd.read_csv("../01_data/SMSSpamCollection.csv", header=None)
     # df_data = df_data.iloc[:, :2] # I can't use this bc of .iloc[235, :]
 
     # read in CSV manually
@@ -17,8 +17,11 @@ if __name__ == "__main__":
     with open("../01_data/SMSSpamCollection.csv") as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
+            sms_txt = [txt for txt in row[1:] if str(txt) != '']  # get rid of empty entries
             df_data = df_data.append(
-                {'collection': row[0], 'sms_txt': ','.join(row[i for i, item in enumerate(row[1:]) if item is not None])}, ignore_index=True)
+                {'collection': row[0], 'sms_txt': ','.join(
+                    sms_txt)},
+                ignore_index=True)
 
     # pprint
     pp.pprint(df_data.head())
